86b3ca73bffd5a6b15b85cc8a5e31160b09f12ac
ajaythoram <115677014+ajaythoram@users.noreply.github.com>
2024-02-08 19:50:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.post('/scrape', async (req, res) => {
     fs.createReadStream('company_data.csv').pipe(res);
   } catch (error) {
     console.error('Error during scraping:', error);
-   return res.status(500).json({ success: false, error: `Error occurred for ${companyName}: ${error.message}` });
+   return res.status(500).json({ success: false, error: `Error occurred for }` });
   }
 });
 
